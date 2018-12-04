7a8ca2b272cc9bc10cabebd0843653684cd2d1d2
nicola-bertelli <45416528+nicola-bertelli@users.noreply.github.com>
2018-12-04 20:40:00 +0100

    Update ir_arduino.py

--- raspberry/telecomando/ir_arduino.py
+++ raspberry/telecomando/ir_arduino.py
@@ -1,7 +1,33 @@
 import serial
 import time
-time.sleep(2)
-arduino = serial.Serial('/dev/ttyUSB1',9600)
+import getpass
+import yaml
+
+utente = getpass.getuser()
+if (utente == "root"):
+    print
+    print ("esci da modalita ROOT e riavvia lo script")
+    print ("GRAZIE")
+    print
+    exit()
+
+try:
+    print
+    file_configurazione = "/home/" + utente + "/telecomando/config.yaml"
+    with open(file_configurazione, 'r') as ymlfile:
+        configurazione = yaml.load(ymlfile)
+except:
+    print ("File di configurazione non trovato")
+    exit()
+print ("File di configurazione impostato " + file_configurazione)
+print
+
+# LEGGO LA PORTA SERIALE
+porta_seriale = configurazione['node_arduino']['porta_seriale']
+print ('porta seriale utilizzata ' + str(porta_seriale))
+
+arduino = serial.Serial(porta_seriale, 9600)
+
 
 while 1:
 	if(arduino.inWaiting()>0):
