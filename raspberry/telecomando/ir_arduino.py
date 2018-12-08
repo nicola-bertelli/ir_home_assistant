08070205bfe82aa7a37ee67e45a8f68b4139cd99
nicola-bertelli <45416528+nicola-bertelli@users.noreply.github.com>
2018-12-08 21:59:16 +0100

    Update ir_arduino.py

--- raspberry/telecomando/ir_arduino.py
+++ raspberry/telecomando/ir_arduino.py
@@ -1,10 +1,19 @@
 import serial
 import time
+import getpass
 import yaml
+
+utente = getpass.getuser()
+if (utente == "root"):
+    print
+    print ("esci da modalita ROOT e riavvia lo script")
+    print ("GRAZIE")
+    print
+    exit()
 
 try:
     print
-    file_configurazione = "/homepi/telecomando/config.yaml"
+    file_configurazione = "/home/" + utente + "/telecomando/config.yaml"
     with open(file_configurazione, 'r') as ymlfile:
         configurazione = yaml.load(ymlfile)
 except:
@@ -24,3 +33,4 @@ while 1:
 	if(arduino.inWaiting()>0):
 		dati = (arduino.readline()).split('\r')[0]
 		print dati
+
